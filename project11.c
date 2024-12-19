9d7bc035968682325939e1e43d11e3311090100b
Patrick Hung <78626322+TheFishEy@users.noreply.github.com>
2024-12-19 01:59:20 -0800

    used #define for best practice, added loops for multiple data reads in chunks and for multiple clients, closed server socket at end, and formatted.

--- project11.c
+++ project11.c
@@ -6,35 +6,51 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024 // define for better code practice
 #define PORT 46645
 #define LISTEN_BACKLOG 5
 
 void handleConnection(int client_socket) {
-    char buffer[1024];
-    int bytes_read = read(client_socket, buffer, sizeof(buffer));
+  char buffer[BUFFER_SIZE];
+  int bytes_read;
+
+  // loop to for multiple chunks of data
+  while ((bytes_read = read(client_socket, buffer, sizeof(buffer))) > 0) {
     printf("Received: %s\n", buffer);
-    write(client_socket, buffer, bytes_read);
+  }
 }
 
 int main(int argc, char *argv[]) {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+  int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in server_address;
-    memset(&server_address, '\0', sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(PORT);
+  struct sockaddr_in server_address;
 
-    int result;
+  // use the memset function to clear garbage values
+  memset(&server_address, '\0', sizeof(server_address));
 
-    result = bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
-    result = listen(server_socket, LISTEN_BACKLOG);
+  server_address.sin_family = AF_INET;
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_address.sin_port = htons(PORT);
 
-    struct sockaddr_in client_address;
-    socklen_t client_address_len = sizeof(client_address);
+  int result;
 
-    int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_address_len);
+  result = bind(server_socket, (struct sockaddr *)&server_address,
+                sizeof(server_address));
+  result = listen(server_socket, LISTEN_BACKLOG);
+
+  struct sockaddr_in client_address;
+  socklen_t client_address_len = sizeof(client_address);
+
+  int client_socket;
+
+  // loop for multiple client connections
+  while (
+      (client_socket = accept(server_socket, (struct sockaddr *)&client_address,
+                              &client_address_len)) >= 0) {
     handleConnection(client_socket);
+    close(client_socket); // close connection
+  }
 
-    return 0;
+  close(server_socket); // close server socket
+  return 0;
 }
